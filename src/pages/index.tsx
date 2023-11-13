1e0046f38125b0b75b21af276ce3f5bc20b04b19
voniu <4230722@qq.com>
2023-11-14 01:26:16 +0800

    chore: css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,16 +11,15 @@ import AlarmList from '@/components/alarmList';
 import AlarmDetail from '@/components/alarmDetail';
 import SingleRTV from '@/components/singleRTV';
 import GridRTV from '@/components/gridRTV';
+
 export default function HomePage() {
   return (
     <div className="data_body">
-      <div style={{ height: 60 }}></div>
       <div className="index_tabs">
-
         <div className="inner" style={{ height: '100%' }}>
           <div className="left_cage">
-            <div className="dataAllBorder01 cage_cl" style={{ marginTop: '9%', height: '24%' }}>
-              <video muted autoPlay loop src={blueVideo} className="dataAllBorder02 video_cage" />
+            <div className="dataAllBorder01 cage_cl" style={{ height: '24%' }}>
+              <video muted autoPlay loop src={blueVideo} className="dataAllBorder01 video_cage" />
             </div>
             <div className="dataAllBorder01 cage_cl" style={{ marginTop: '1.5%', height: '38%' }}>
               <GridRTV />
@@ -32,8 +31,8 @@ export default function HomePage() {
 
           <div className="center_cage">
             <div className="dataAllBorder01 cage_cl"
-              style={{ marginTop: '3.5%', height: '62.7%', position: 'relative' }}>
-              <div className="dataAllBorder02" style={{ position: 'relative', overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
+              style={{ height: '62.7%', position: 'relative' }}>
+              <div className="dataAllBorder01" style={{ position: 'relative', overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
                 <div className="map_title_box" style={{ height: 32 }}>
                   <div className="map_title_innerbox">
                     <div className="map_title" style={{ backgroundImage: `url(${firstTitlePng})` }}>实时布防地图</div>
@@ -45,22 +44,24 @@ export default function HomePage() {
               </div>
             </div>
             <div className="dataAllBorder01 cage_cl" style={{ marginTop: '0.6%', height: '32.1%' }}>
-              <div className="dataAllBorder02" id="map_title_innerbox1"
+              <div className="dataAllBorder01" id="map_title_innerbox1"
                 style={{ position: 'relative', overflow: 'auto' }}>
                 <div className="map_title_box">
                   <div className="map_title_innerbox">
                     <div className="map_title" style={{ backgroundImage: `url(${secondTitlePng})` }}>
                       防区数据</div>
-                    <FiberList />
                   </div>
+                </div>
+                <div style={{ height: '89%' }}>
+                  <FiberList />
                 </div>
               </div>
             </div>
           </div>
 
           <div className="right_cage">
-            <div className="dataAllBorder01 cage_cl" style={{ marginTop: '9%', height: '24%' }}>
-              <div className="dataAllBorder02">
+            <div className="dataAllBorder01 cage_cl" style={{ height: '24%' }}>
+              <div className="dataAllBorder01" style={{ display: 'flex', flexDirection: 'column' }}>
                 <DigitalFlop />
               </div>
             </div>
