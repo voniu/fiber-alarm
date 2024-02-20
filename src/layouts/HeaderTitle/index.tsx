3c7dbba79584c76b59fe2752a7d22835e4f29cfc
bindStudy <2728103295@qq.com>
2024-02-20 19:29:32 +0800

    fix 'logo 图标'

--- src/layouts/HeaderTitle/index.tsx
+++ src/layouts/HeaderTitle/index.tsx
@@ -1,10 +1,10 @@
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/logo_new.png";
 import styles from "./index.less";
 export default function HeaderTitle() {
   return (
     <div className={styles["logo"]}>
       <div className={styles["logo-img"]}>
-        <img style={{ height: 40 }} src={Logo} />
+        <img style={{ height: "100%" }} src={Logo} />
       </div>
       <span className={styles["header-title"]}>Duty Manage System</span>
     </div>
